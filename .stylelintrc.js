15af3b06a1c6c24633e6ff2195a37b161d03a662
Oleg Arbuzov <oleg.arbuzov@rt.ru>
2024-12-12 15:59:42 +0300

    chore: инициализация проекта

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: 'stylelint-config-standard-scss',
+    rules: {
+        'declaration-no-important': null,
+        indentation: [4],
+        'string-quotes': 'single',
+    },
+};
---
+++ webpack.config.common.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './index.tsx',
+    output: {
+        filename: '[name].[contenthash].js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js', '.jsx', '.scss'],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+        }),
+        // new CopyPlugin({
+        //   patterns: [
+        //     {
+        //       from: path.resolve(__dirname, 'public/favicon.png'),
+        //       to: path.resolve(__dirname, 'dist'),
+        //     },
+        //   ],
+        // }),
+        new MiniCssExtractPlugin({
+            filename: '[name].[contenthash].css',
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.module\.s[ac]ss$/i,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                namedExport: false,
+                                exportLocalsConvention: 'as-is',
+                            },
+                        },
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            postcssOptions: {
+                                plugins: [require('postcss-preset-env')],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                exclude: /\.module\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.[tj]sx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+};
