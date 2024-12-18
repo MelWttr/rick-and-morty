8480a7ebbd0d018225c46c5074e542248265fed8
Oleg Arbuzov <oleg.arbuzov@rt.ru>
2024-12-19 00:25:21 +0300

    feat: добавляет авторизацию и форму авторизации - добавляет котекст для авторизации - добавляет кнопку Войти/Выйти - добавляет форму авторизации

---
+++ src/components/SignButton/SignButton.tsx
@@ -0,0 +1,28 @@
+import {
+    FC, memo,
+} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../../context/AuthProvider';
+import { AppRoute } from '../../constants';
+
+export const SignButtonComponent: FC = () => {
+    const auth = useAuth();
+    const navigate = useNavigate();
+    const isLoggedIn: boolean = Boolean(auth?.login);
+
+    const handleCLick = () => {
+        if (isLoggedIn) {
+            auth?.signOut?.(() => navigate(AppRoute.Root));
+        } else {
+            navigate(AppRoute.Login);
+        }
+    };
+
+    return (
+        <button type='button' onClick={handleCLick}>
+            {isLoggedIn ? 'Выйти' : 'Войти'}
+        </button>
+    );
+};
+
+export const SignButton = memo(SignButtonComponent);
